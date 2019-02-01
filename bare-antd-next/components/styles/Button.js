419d8b5efaf19b34fc9d79b7b9f0850637053dba
Darren Poon <dyhpoon@gmail.com>
2019-02-01 11:37:47 +0800

    [bare-antd-next] init

---
+++ bare-antd-next/components/styles/Button.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export default styled.button`
+  display: block;
+  background-color: ${props => props.color};
+  text-transform: uppercase;
+  color: white;
+`
---
+++ news-antd-next/pages/news.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class News extends Component {
+  static async getInitalProps({ query, res }) {
+    
+  }
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default News;
