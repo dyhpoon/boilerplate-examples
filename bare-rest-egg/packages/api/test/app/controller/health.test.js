c1c180c9722799c2a3bd744b0a66927fe3465f63
Darren Poon <dyhpoon@gmail.com>
2019-02-06 16:20:27 +0800

    [bare-test-egg] add test for GET /healthcheck

---
+++ bare-rest-egg/packages/api/test/app/controller/health.test.js
@@ -0,0 +1,20 @@
+const { mock, assert } = require('egg-mock/bootstrap')
+
+describe('test/app.controller/health.test.js', () => {
+  let app
+
+  before(async () => {
+    app = mock.app()
+    await app.ready()
+  })
+
+  after(() => app.close())
+  
+  it('(GET /healthcheck) should get an uptime', async () => {
+    const result = await app
+      .httpRequest()
+      .get('/healthcheck')
+      .expect(200)
+    assert(result.body.uptime)
+  })
+})
